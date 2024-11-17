26033c19f38921e097e479525656cc8a86e1fa97
Akash <akashdhillon4380@gmail.com>
2024-11-17 12:27:57 -0800

    Added sorting for strings

--- SortableArray.java
+++ SortableArray.java
@@ -1,7 +1,7 @@
 public class SortableArray {
     public static void main(String[] args) {
-        int[] numbers = {4, 2, 7, 1, 9};
-        Arrays.sort(numbers);  
-        System.out.println("Sorted integers: " + Arrays.toString(numbers));
+        String[] names = {"Akash", "Dhillon", "Jason", "Jacob"};
+        Arrays.sort(names);  
+        System.out.println("Sorted names: " + Arrays.toString(names));
     }
 }
