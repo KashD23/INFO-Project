86bbc34bb26b991aaa857f4cedc40523557c0a28
Akash <akashdhillon4380@gmail.com>
2024-11-17 00:29:10 -0800

    Added sorting for integers

--- SortableArray.java
+++ SortableArray.java
@@ -1,9 +1,7 @@
-import java.util.Arrays;
-
 public class SortableArray {
     public static void main(String[] args) {
         int[] numbers = {4, 2, 7, 1, 9};
         Arrays.sort(numbers);  
-        System.out.println("Sorted array: " + Arrays.toString(numbers));
+        System.out.println("Sorted integers: " + Arrays.toString(numbers));
     }
 }
